cd3a46623f040e4e22edf3942eb65ac5250a5721
Michael Bressler <53796824+mibressler@users.noreply.github.com>
2022-06-07 13:35:08 +0200

    Update SidebarNav.tsx

--- components/SidebarNav.tsx
+++ components/SidebarNav.tsx
@@ -35,7 +35,7 @@ export const SidebarNav = () => {
           className="h4-mktg color-text-primary no-underline no-wrap flex-auto"
         >
           {/*{t('github_docs')} */}
-          Social Media Leitfadens
+          Social Media Leitfaden
         </Link>
       </div>
       <nav>
