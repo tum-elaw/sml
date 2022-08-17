39b2bbe45ed7e7d26eeabae626fed816dc989eb9
He1m0 <105716248+He1m0@users.noreply.github.com>
2022-08-17 13:49:20 +0200

    header now scales with window size

--- components/landing/HfPHeader.tsx
+++ components/landing/HfPHeader.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from 'react'
 type Props = ComponentProps<'svg'>
 export function HfPHeader(props: Props) {
   return (
-    <img src='./assets/images/sml/headersml.jpg' height="500" alt="Like"></img>
+    <img src='./assets/images/sml/headersml.jpg' className="width-fit" alt="Like"></img>
     // SML TODO
     // height should be relative to window/display size. How would that be implemented
     // match size to parent div so there are no borders?
