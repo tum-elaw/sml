51b22c11e0c42b161521bdb7bf27ff2ee720dc3b
He1m0 <105716248+He1m0@users.noreply.github.com>
2022-09-04 16:00:41 +0200

    modified Gesetzestext arrangement

--- components/SupportSection.tsx
+++ components/SupportSection.tsx
@@ -17,26 +17,31 @@ export const SupportSection = () => {
   return (
     <section className="mt-lg-9 py-7 px-3 px-md-6 no-print color-bg-tertiary">
       <div className="container-xl gutter-lg-spacious clearfix">
-        <div className="col-4 col-lg-4 col-xl-2 float-left">
+        <div className="col-7 float-left">
         <h3 className="mb-2 f4">{"Gesetzestexte"}</h3>
-        <a id="GG" href="https://www.gesetze-im-internet.de/gg/" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`GG`}
-        </a>
-        <a id="NetzDG" href="https://www.gesetze-im-internet.de/netzdg/index.html" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`NetzDG`}
-        </a>
-        <a id="UrhG" href="https://www.gesetze-im-internet.de/urhg/" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`UrhG`}
-        </a>
-        <a id="KUG" href="https://www.gesetze-im-internet.de/kunsturhg/index.html" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`KUG`}
-        </a>
-        <a id="DSGVO" href="https://dejure.org/gesetze/DSGVO'" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`DSGVO`}
-        </a>
-        <a id="StGB" href="https://www.gesetze-im-internet.de/stgb/" target="_blank" className="btn btn-outline mr-4 mt-2">
-          {t`StGB`}
-        </a>
+        <div>
+          <a id="GG" href="https://www.gesetze-im-internet.de/gg/" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`GG`}
+          </a>
+          <a id="NetzDG" href="https://www.gesetze-im-internet.de/netzdg/index.html" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`NetzDG`}
+          </a>
+          <a id="UrhG" href="https://www.gesetze-im-internet.de/urhg/" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`UrhG`}
+          </a>
+          <a id="KUG" href="https://www.gesetze-im-internet.de/kunsturhg/index.html" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`KUG`}
+          </a>
+          <a id="DSGVO" href="https://dejure.org/gesetze/DSGVO'" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`DSGVO`}
+          </a>
+          <a id="StGB" href="https://www.gesetze-im-internet.de/stgb/" target="_blank" className="btn btn-outline mr-4 mt-2">
+            {t`StGB`}
+          </a>
+        </div>
+        <div>
+          
+        </div>
         </div>
         <div className="col-12 col-lg-12 col-xl-4 float-right">
           <Support />
